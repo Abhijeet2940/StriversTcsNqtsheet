da9bb21faa38aebd5041daecca87d084c8950679
Abhijeet Kumar <88205898+Abhijeet2940@users.noreply.github.com>
2023-10-05 23:37:35 +0530

    Add files via upload

---
+++ number/checkpositiveorNegative.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a=-0;
+if(a>0){
+    cout<<"positive";
+}else if(a<0){
+    cout<<"negative";
+}else{
+    cout<<"zero";
+}
+return 0;
+}
